6e9b4ea717c3d1db47f366ea51be94ee9b30224f
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-10-19 13:24:14 +0300

    fix(ut-front): tidy up code

--- react.js
+++ react.js
@@ -4,12 +4,12 @@ var when = require('when');
 var self;
 
 module.exports = {
-    createClass : function(spec){
+    createClass : function(spec) {
         return React.createClass(spec);
     },
-    createElement : function(type, props, children){
-        if(props.type == 'button') {
-            if(!this.checkPermission(props.action)) {
+    createElement : function(type, props, children) {
+        if (props.type === 'button') {
+            if (!this.checkPermission(props.action)) {
                 return '';
             }
         }
@@ -18,7 +18,7 @@ module.exports = {
                 props = {};
             }
             var members = Array.prototype.slice.call(arguments, 2);
-            switch(type.Class) {
+            switch (type.Class) {
                 case 'Window':
                     props.items = members;
                     break;
@@ -40,25 +40,25 @@ module.exports = {
                                 return this;
                             }.bind(this)
                         });
-                    }
+                    };
                 }.bind(this))(props[prop]);
                 delete props[prop];
             }
         }.bind(this));
 
-        var result=type.create(props);
-        props.ref && ((props.$owner.refs || (props.$owner.refs={}))[props.ref] = result);
+        var result = type.create(props);
+        props.ref && ((props.$owner.refs || (props.$owner.refs = {}))[props.ref] = result);
         return result;
     },
     frontEnd : function() {
-        return isc;
+        return window.isc;
     },
-    request: function( opcode, params ){
+    request: function(opcode, params) {
         return this.bus.importMethod(opcode)(params)
-            .catch(function(error){
+            .catch(function(error) {
                 console.log(opcode, params, error);
-                if(self.bus.config.identity && error.code == self.bus.config.identity.errorCode) {
-                    isc.warn((error.errorPrint || error.message) + " Please relogin!", function(){
+                if (self.bus.config.identity && error.code === self.bus.config.identity.errorCode) {
+                    window.isc.warn((error.errorPrint || error.message) + ' Please relogin!', function() {
                         location.reload();
                     });
                 } else {
@@ -67,16 +67,16 @@ module.exports = {
             });
     },
     checkPermission: function(action) {
-        if( this.bus.config.session && Array.isArray(this.bus.config.session.permissions)) {
-            if(action && this.bus.config.session.permissions.indexOf(action) === -1){
+        if (this.bus.config.session && Array.isArray(this.bus.config.session.permissions)) {
+            if (action && this.bus.config.session.permissions.indexOf(action) === -1) {
                 return false;
             }
         }
         return true;
     },
-    checkRights: function(items){
+    checkRights: function(items) {
         if (items && Array.isArray(items)) {
-            for (var key = 0, len = items.length; key < len; key++) {
+            for (var key = 0, len = items.length; key < len; key += 1) {
                 if (items[key] && items[key].type === 'button' && !this.checkPermission(items[key].action)) {
                     items.splice(key, 1);
                 }
@@ -84,33 +84,33 @@ module.exports = {
         }
         return items;
     },
-    openPage: function(nameSpace){
-        this.bus.importMethod(nameSpace+'.ui.render')(this);
+    openPage: function(nameSpace) {
+        this.bus.importMethod(nameSpace + '.ui.render')(this);
     },
     init: function(bus) {
         self = this;
         this.bus = bus;
-        isc.defineClass("RPCDataSource", "RestDataSource");
-        isc.RPCDataSource.addProperties({
-            dataFormat:"json",
-            transformRequest:function(request){
+        window.isc.defineClass('RPCDataSource', 'RestDataSource');
+        window.isc.RPCDataSource.addProperties({
+            dataFormat:'json',
+            transformRequest:function(request) {
                 var data = {};
-                var params = this.Super("transformRequest", arguments);
-                if(this.defaultParams) {
+                this.Super('transformRequest', arguments);
+                if (this.defaultParams) {
                     data = JSON.parse(this.defaultParams);
                 }
-                data = _.assign({}, this.Super("transformRequest", arguments), data);
-                request.dataProtocol='clientCustom';
-                self.request(this.dataURL+'.'+request.operationType, data)
-                    .then(function(result){
+                data = _.assign({}, this.Super('transformRequest', arguments), data);
+                request.dataProtocol = 'clientCustom';
+                self.request(this.dataURL + '.' + request.operationType, data)
+                    .then(function(result) {
                         this.processResponse(request.requestId,{status:0,data:result});
                     }.bind(this))
-                    .catch(function(error){
-                        isc.warn(error.errorPrint || error.message);
+                    .catch(function(error) {
+                        window.isc.warn(error.errorPrint || error.message);
                         this.processResponse(request.requestId,{status:0,data:[]});
                     }.bind(this));
                 return data;
             }
-        })
+        });
     }
-}
+};
