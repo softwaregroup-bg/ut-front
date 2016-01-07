6a597b560428aa544df7e44b3148b6ee4a35e7f2
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2016-01-07 11:24:51 +0200

    fix(typos)

--- index.js
+++ index.js
@@ -59,53 +59,54 @@ module.exports = function(config) {
             var main = config.main;
             delete config.main;
             lasso.configure(assign({
-                plugins: [
-                    {
-                        plugin: 'lasso-require',
-                        config: {
-                            builtins: {
-                                'os': 'os-browserify',
-                                'fs': require.resolve('ut-bus/browser/fs')
+                    plugins: [
+                        {
+                            plugin: 'lasso-require',
+                            config: {
+                                builtins: {
+                                    'os': 'os-browserify',
+                                    'fs': 'ut-bus/browser/fs'
+                                }
                             }
-                        }
-                    },
-                    'lasso-jsx',
-                    'lasso-marko'
-                ],
-                urlPrefix: '/s/cache',
-                outputDir: cachePath,
-                cacheDir: lassoCache,
-                fingerprintsEnabled: false,
-                minifyJS: true,
-                resolveCssUrls: true,
-                bundlingEnabled: true
-            }, config));
+                        },
+                        'lasso-jsx',
+                        'lasso-marko'
+                    ],
+                    urlPrefix: '/s/cache',
+                    outputDir: cachePath,
+                    cacheDir: lassoCache,
+                    fingerprintsEnabled: false,
+                    minifyJS: true,
+                    resolveCssUrls: true,
+                    bundlingEnabled: true
+                }, config)
+            );
             return when.promise(function(resolve, reject) {
                 lasso.lassoPage({
-                    name: 'app',
-                    dependencies: [
-                        'require-run: ' + main
-                    ],
-                    from: __dirname
-                },
-                function(err, result) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve('<!doctype html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">' +
-                            '<meta name="viewport" content="width=device-width, initial-scale=1"><script>var isomorphicDir="isomorphic/";</script>' +
-                            '<script src="isomorphic/system/modules/ISC_Core.js"></script><script src="isomorphic/system/modules/ISC_Foundation.js">' +
-                            '</script><script src="isomorphic/system/modules/ISC_Containers.js"></script>' +
-                            '<script src="isomorphic/system/modules/ISC_Grids.js"></script><script src="isomorphic/system/modules/ISC_Forms.js"></script>' +
-                            '<script src="isomorphic/system/modules/ISC_DataBinding.js"></script><script src="isomorphic/skins/Enterprise/load_skin.js">' +
-                            '</script><meta charset="UTF-8"><script type="text/javascript">global=window;' +
-                            '</script><title>UnderTree</title>' +
-                            result.getHeadHtml() +
-                            '</head><body>' +
-                            result.getBodyHtml() +
-                            '</body></html>');
-                    }
-                });
+                        name: 'app',
+                        dependencies: [
+                            'require-run: ' + main
+                        ],
+                        from: __dirname
+                    },
+                    function(err, result) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve('<!doctype html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">' +
+                                '<meta name="viewport" content="width=device-width, initial-scale=1"><script>var isomorphicDir="isomorphic/";</script>' +
+                                '<script src="isomorphic/system/modules/ISC_Core.js"></script><script src="isomorphic/system/modules/ISC_Foundation.js">' +
+                                '</script><script src="isomorphic/system/modules/ISC_Containers.js"></script>' +
+                                '<script src="isomorphic/system/modules/ISC_Grids.js"></script><script src="isomorphic/system/modules/ISC_Forms.js"></script>' +
+                                '<script src="isomorphic/system/modules/ISC_DataBinding.js"></script><script src="isomorphic/skins/Enterprise/load_skin.js">' +
+                                '</script><meta charset="UTF-8"><script type="text/javascript">global=window;' +
+                                '</script><title>UnderTree</title>' +
+                                result.getHeadHtml() +
+                                '</head><body>' +
+                                result.getBodyHtml() +
+                                '</body></html>');
+                        }
+                    });
             });
         }
     };
