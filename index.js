1419e1762be9b737f0df3c97a0199371dca8c4fe
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-01-13 10:36:08 +0200

    fix: cleanup

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function findRootDir(dirname) {
     }
 }
 
-module.exports = function(config) {
+module.exports = function(moduleConfig) {
     var bus;
     var cachePath;
     var lassoCache;
@@ -36,7 +36,7 @@ module.exports = function(config) {
             lassoCache = path.resolve(bus.config.workDir, 'lasso');
         },
         start: function() {
-            this && this.registerRequestHandler && this.registerRequestHandler([{
+            return this && this.registerRequestHandler && this.registerRequestHandler([{
                 method: 'GET',
                 path: '/s/sc/{p*}',
                 handler: {
@@ -61,7 +61,7 @@ module.exports = function(config) {
                 method: 'GET',
                 path: '/pack',
                 handler: function(request, reply) {
-                    result.pack(config)
+                    result.pack(moduleConfig)
                         .then(function() {
                             reply.redirect('/s/sc/index.html');
                         });
@@ -70,9 +70,9 @@ module.exports = function(config) {
                 method: 'GET',
                 path: '/s/sc/debug.html',
                 handler: function(request, reply) {
-                    result.pack(assign({minifyJS: false, bundlingEnabled: false}, config))
-                        .then(function(result) {
-                            reply(result);
+                    result.pack(assign({minifyJS: false, bundlingEnabled: false}, moduleConfig))
+                        .then(function(pack) {
+                            reply(pack);
                         });
                 }
             }]);
@@ -88,9 +88,9 @@ module.exports = function(config) {
                         config: {
                             builtins: {
                                 'os': 'os-browserify',
-                                'fs': require.resolve('ut-bus/browser/fs')
-                            },
-                            rootDir: root
+                                'fs': require.resolve('ut-bus/browser/fs'),
+                                'stream': require.resolve('stream-browserify')
+                            }
                         }
                     },
                     'lasso-jsx',
@@ -102,36 +102,38 @@ module.exports = function(config) {
                 fingerprintsEnabled: false,
                 minifyJS: true,
                 resolveCssUrls: true,
-                projectRoot: root,
                 bundlingEnabled: true
             }, config), root);
 
             return when.promise(function(resolve, reject) {
                 lasso.lassoPage({
-                        name: 'app',
-                        dependencies: [
-                            'require-run: ' + main
-                        ],
-                        from: __dirname
-                    },
-                    function(err, result) {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            resolve('<!doctype html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">' +
-                                '<meta name="viewport" content="width=device-width, initial-scale=1"><script>var isomorphicDir="isomorphic/";</script>' +
-                                '<script src="isomorphic/system/modules/ISC_Core.js"></script><script src="isomorphic/system/modules/ISC_Foundation.js">' +
-                                '</script><script src="isomorphic/system/modules/ISC_Containers.js"></script>' +
-                                '<script src="isomorphic/system/modules/ISC_Grids.js"></script><script src="isomorphic/system/modules/ISC_Forms.js"></script>' +
-                                '<script src="isomorphic/system/modules/ISC_DataBinding.js"></script><script src="isomorphic/skins/Enterprise/load_skin.js">' +
-                                '</script><meta charset="UTF-8"><script type="text/javascript">global=window;' +
-                                '</script><title>UnderTree</title>' +
-                                result.getHeadHtml() +
-                                '</head><body>' +
-                                result.getBodyHtml() +
-                                '</body></html>');
-                        }
-                    });
+                    name: 'app',
+                    dependencies: [
+                        'require-run: ' + main
+                    ],
+                    from: __dirname
+                },
+                function(err, results) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve('<!doctype html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">' +
+                            '<meta name="viewport" content="width=device-width, initial-scale=1"><script>var isomorphicDir="isomorphic/";</script>' +
+                            '<script src="isomorphic/system/modules/ISC_Core.js"></script>' +
+                            '<script src="isomorphic/system/modules/ISC_Foundation.js"></script>' +
+                            '<script src="isomorphic/system/modules/ISC_Containers.js"></script>' +
+                            '<script src="isomorphic/system/modules/ISC_Grids.js"></script>' +
+                            '<script src="isomorphic/system/modules/ISC_Forms.js"></script>' +
+                            '<script src="isomorphic/system/modules/ISC_DataBinding.js">' +
+                            '</script><script src="isomorphic/skins/Enterprise/load_skin.js">' +
+                            '</script><meta charset="UTF-8"><script type="text/javascript">global=window;' +
+                            '</script><title>UnderTree</title>' +
+                            results.getHeadHtml() +
+                            '</head><body>' +
+                            results.getBodyHtml() +
+                            '</body></html>');
+                    }
+                });
             });
         }
     };
