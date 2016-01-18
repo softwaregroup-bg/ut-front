c2157668c6e0e50a6c375cd52e0b720910964ead
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-01-18 14:14:27 +0200

    fix linting

--- index.js
+++ index.js
@@ -2,28 +2,6 @@ var path = require('path');
 var lasso = require('lasso');
 var when = require('when');
 var assign = require('lodash/object/assign');
-var fs = require('fs');
-var serverRequire = require;
-
-function findRootDir(dirname) {
-    if (dirname === '' || dirname === '/') {
-        return null;
-    }
-
-    var packagePath = path.join(dirname, 'package.json');
-
-    if (dirname.indexOf('node_modules') === -1 && fs.existsSync(packagePath)) {
-        return dirname;
-    }
-
-    var parentDirname = path.dirname(dirname);
-
-    if (parentDirname !== dirname) {
-        return findRootDir(parentDirname);
-    } else {
-        return null;
-    }
-}
 
 module.exports = function(moduleConfig) {
     var bus;
