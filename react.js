1a45335c8f28b9f6a9c1eeaa7837e75f6812b4e5
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-01-19 23:09:19 +0200

    streamline frontend structure

--- react.js
+++ react.js
@@ -1,14 +1,40 @@
 var React = require('react/lib/ReactClass');
+var PropTypes = require('react/lib/ReactPropTypes');
 var _ = require('lodash');
 var when = require('when');
-var self;
+
+window.isc.defineClass('RPCDataSource', 'RestDataSource');
+window.isc.RPCDataSource.addProperties({
+    dataFormat: 'json',
+    transformRequest: function(request) {
+        var data = {};
+        this.Super('transformRequest', arguments);
+        if (this.defaultParams) {
+            data = JSON.parse(this.defaultParams);
+        }
+        data = _.assign({}, this.Super('transformRequest', arguments), data);
+        request.dataProtocol = 'clientCustom';
+        module.exports.request(this.dataURL + '.' + request.operationType, data)
+            .then(function(result) {
+                this.processResponse(request.requestId, {status: 0, data: (Array.isArray(result) && Array.isArray(result[0])) ? result[0] : result});
+            }.bind(this))
+            .catch(function(error) {
+                window.isc.warn(error.errorPrint || error.message);
+                if (request.operationType === 'fetch') {
+                    this.processResponse(request.requestId, {status: 0, data: []});
+                }
+            }.bind(this));
+        return data;
+    }
+});
 
 module.exports = {
     createClass: function(spec) {
         return React.createClass(spec);
     },
+    PropTypes: PropTypes,
     createElement: function(type, props, children) {
-        if (props.type === 'button' || props.ut5Action) {
+        if (props && (props.type === 'button' || props.ut5Action)) {
             if (!this.checkPermission(props.ut5Action || props.action)) {
                 return '';
             }
@@ -27,7 +53,7 @@ module.exports = {
                     break;
             }
         }
-        Object.keys(props).forEach(function(prop) {
+        props && Object.keys(props).forEach(function(prop) {
             if (prop.endsWith('_action')) {
                 props[prop.slice(0, -7)] = (function(action) {
                     var bus = this.bus;
@@ -46,9 +72,9 @@ module.exports = {
             }
         }.bind(this));
 
-        var result = type.create(props);
-        props.ref && ((props.$owner.refs && Object.isFrozen(props.$owner.refs)) || (!props.$owner.refs)) && (props.$owner.refs = {});
-        props.ref && (props.$owner.refs[props.ref] = result);
+        var result = type.create ? type.create(props) : (new type(props)).render();
+        props && props.ref && ((props.$owner.refs && Object.isFrozen(props.$owner.refs)) || (!props.$owner.refs)) && (props.$owner.refs = {});
+        props && props.ref && (props.$owner.refs[props.ref] = result);
         return result;
     },
     frontEnd: function() {
@@ -59,7 +85,7 @@ module.exports = {
             return this.bus.importMethod(opcode)(params)
                 .catch(function(error) {
                     console.log(opcode, params, error);
-                    if (self.bus.config.identity && error.code === self.bus.config.identity.errorCode) {
+                    if (module.exports.bus.config.identity && error.code === module.exports.bus.config.identity.errorCode) {
                         window.isc.warn((error.errorPrint || error.message) + ' Please relogin!', function() {
                             window.location.reload();
                         });
@@ -111,31 +137,6 @@ module.exports = {
         }
     },
     init: function(bus) {
-        self = this;
         this.bus = bus;
-        window.isc.defineClass('RPCDataSource', 'RestDataSource');
-        window.isc.RPCDataSource.addProperties({
-            dataFormat: 'json',
-            transformRequest: function(request) {
-                var data = {};
-                this.Super('transformRequest', arguments);
-                if (this.defaultParams) {
-                    data = JSON.parse(this.defaultParams);
-                }
-                data = _.assign({}, this.Super('transformRequest', arguments), data);
-                request.dataProtocol = 'clientCustom';
-                self.request(this.dataURL + '.' + request.operationType, data)
-                    .then(function(result) {
-                        this.processResponse(request.requestId, {status: 0, data: (Array.isArray(result) && Array.isArray(result[0])) ? result[0] : result});
-                    }.bind(this))
-                    .catch(function(error) {
-                        window.isc.warn(error.errorPrint || error.message);
-                        if (request.operationType === 'fetch') {
-                            this.processResponse(request.requestId, {status: 0, data: []});
-                        }
-                    }.bind(this));
-                return data;
-            }
-        });
     }
 };
