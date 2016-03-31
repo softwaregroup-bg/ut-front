8c6ff7429630e7afa0f78df687e6f3f8028ac967
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2016-03-31 13:44:20 +0300

    fix(devtools)

--- react-redux-router/index.js
+++ react-redux-router/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { persistState } from 'redux-devtools';
 import { Route } from 'react-router';
 import assign from 'lodash/object/assign';
 import PageNotFound from './common/PageNotFound.jsx';
+import DevTools from './DevTools';
 
 var store;
 var history;
+const enhancer = compose(
+    applyMiddleware(thunk),
+    DevTools.instrument(),
+    persistState(
+        window.location.href.match(
+            /[?&]debug_session=([^&#]+)\b/
+        )
+    )
+);
 
 export class UtFront extends React.Component {
     constructor(props) {
@@ -20,7 +32,9 @@ export class UtFront extends React.Component {
         if (Object.keys(props.reducers).length) {
             reducers = assign({}, reducers, props.reducers);
         }
-        store = createStore(combineReducers(reducers));
+        store = createStore(combineReducers(
+            reducers
+        ), {}, enhancer);
         history = syncHistoryWithStore(hashHistory, store);
     }
     render() {
@@ -31,6 +45,7 @@ export class UtFront extends React.Component {
                         {this.props.children}
                         <Route path='*' component={PageNotFound}/>
                     </Router>
+                    <DevTools/>
                 </div>
             </Provider>
         );
