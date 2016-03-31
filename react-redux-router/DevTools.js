32438674fe1e637ef62335b264516ac0e1ac7a1c
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2016-03-31 14:00:39 +0300

    fix(devtools)

--- react-redux-router/DevTools.js
+++ react-redux-router/DevTools.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import assign from 'lodash/object/assign';
+import { routerReducer } from 'react-router-redux';
+import { persistState } from 'redux-devtools';
+import thunk from 'redux-thunk';
+import DevTools from './DevTools';
 
-export default createDevTools(
-  <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-w'>
-    <LogMonitor />
-  </DockMonitor>
+const enhancer = compose(
+    applyMiddleware(thunk),
+    DevTools.instrument(),
+    persistState(
+        window.location.href.match(
+            /[?&]debug_session=([^&#]+)\b/
+        )
+    )
 );
+
+export function Store(reducers) {
+    var mixedReducers = {
+        routing: routerReducer
+    };
+    if (Object.keys(reducers).length) {
+        mixedReducers = assign({}, mixedReducers, reducers);
+    }
+
+    return createStore(combineReducers(
+        mixedReducers
+    ), {}, enhancer);
+};
--- react-redux-router/index.js
+++ react-redux-router/index.js
@@ -1,40 +1,20 @@
 import React from 'react';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
-import { persistState } from 'redux-devtools';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { Route } from 'react-router';
-import assign from 'lodash/object/assign';
-import PageNotFound from './common/PageNotFound.jsx';
+import PageNotFound from './components/PageNotFound.jsx';
 import DevTools from './DevTools';
+import Store from './Store';
 
 var store;
 var history;
-const enhancer = compose(
-    applyMiddleware(thunk),
-    DevTools.instrument(),
-    persistState(
-        window.location.href.match(
-            /[?&]debug_session=([^&#]+)\b/
-        )
-    )
-);
 
 export class UtFront extends React.Component {
     constructor(props) {
         super(props);
-        var reducers = {
-            routing: routerReducer
-        };
 
-        if (Object.keys(props.reducers).length) {
-            reducers = assign({}, reducers, props.reducers);
-        }
-        store = createStore(combineReducers(
-            reducers
-        ), {}, enhancer);
+        store = Store(props.reducers);
         history = syncHistoryWithStore(hashHistory, store);
     }
     render() {
