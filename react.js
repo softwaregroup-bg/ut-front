6be0a1e4a3d0a6456fd268d2f3e4f78f940b3e4b
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2016-04-01 13:07:28 +0300

    fix(refactor)

--- react.js
+++
@@ -1,6 +0,0 @@
-var error = 'require(\'ut-front/react\') is deprecated,\nplease require(\'ut-front-smartclient/react\') instead';
-if (typeof global.alert === 'function') {
-    global.alert(error);
-} else {
-    throw new Error(error);
-}
