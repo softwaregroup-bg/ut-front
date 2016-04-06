08ecabf4764e7a1a6fb5c279e3025636810aaba7
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2016-04-06 15:14:51 +0300

    fix(webpack): source maps loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var webpack = require('webpack');
-var BellOnBundlerErrorPlugin = require('bell-on-bundler-error-plugin')
+var BellOnBundlerErrorPlugin = require('bell-on-bundler-error-plugin');
 
 module.exports = {
-    devtool: 'eval-source-map',
+    // devtool: 'eval-inline-source-map',
     entry: {
         index: './browser/index.js'
     },
@@ -18,6 +18,7 @@ module.exports = {
     resolve: {
         modules: ['node_modules', 'dev'] // https://github.com/webpack/webpack/issues/2119#issuecomment-190285660
     },
+    bail: true,
     module: {
         loaders: [
             {test: /\.jsx?$/, exclude: /(node_modules(\\|\/)(?!(impl|ut)\-).)/, loader: 'babel', query: {presets: ['react', 'es2015-without-strict']}},
@@ -25,7 +26,8 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.IgnorePlugin(/^(app|browser\-window|global\-shortcut|crash\-reporter|protocol|dgram|JSONStream|inert|hapi)$/),
+        new webpack.SourceMapDevToolPlugin({filename: 'bundle.js.map', moduleFilenameTemplate: '[absolute-resource-path]', fallbackModuleFilenameTemplate: '[absolute-resource-path]'}),
+        new webpack.IgnorePlugin(/^(app|browser\-window|global\-shortcut|crash\-reporter|protocol|dgram|JSONStream|inert|hapi|socket\.io|winston|async|bn\.js|engine\.io|url|glob|mv|minimatch|stream-browserify|browser-request)$/),
         new BellOnBundlerErrorPlugin()
     ]
 };
