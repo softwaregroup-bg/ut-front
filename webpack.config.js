7d5b51ab14074a96daabe933c59d1eb74dd91b34
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2016-05-18 11:33:07 +0300

    fix: dependecy update + babel cache

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,15 @@ module.exports = function(params) {
         bail: true,
         module: {
             loaders: [
-                {test: /\.jsx?$/, exclude: /(node_modules(\\|\/)(?!(impl|ut)\-).)/, loaders: ['babel?{presets: [\'react\', \'es2015-without-strict\', \'stage-0\']}']},
+                {
+                    test: /\.jsx?$/,
+                    exclude: /(node_modules(\\|\/)(?!(impl|ut)\-).)/,
+                    loader: 'babel',
+                    query: {
+                        presets: ['react', 'es2015-without-strict', 'stage-0'],
+                        cacheDirectory: true
+                    }
+                },
                 {test: /\.json$/, loader: 'json'},
                 {test: /.*\.(gif|png|jpe?g|svg)$/i, loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}']},
                 {test: /\.css$/, loaders: ['style?sourceMap', 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]']}
@@ -33,6 +41,7 @@ module.exports = function(params) {
         },
         plugins: [
             new webpack.SourceMapDevToolPlugin({filename: 'bundle.js.map', moduleFilenameTemplate: '[absolute-resource-path]', fallbackModuleFilenameTemplate: '[absolute-resource-path]'}),
+            // new webpack.optimize.DedupePlugin(),
             new webpack.IgnorePlugin(/^(app|browser\-window|global\-shortcut|crash\-reporter|protocol|dgram|JSONStream|inert|hapi|socket\.io|winston|async|bn\.js|engine\.io|url|glob|mv|minimatch|stream-browserify|browser-request)$/),
             new BellOnBundlerErrorPlugin()
         ]
