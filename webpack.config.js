0e1d01c20144a13556bdf100af06912da038bc3b
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-05-19 11:47:35 +0300

    feat: embed fonts

--- webpack.config.js
+++ webpack.config.js
@@ -27,27 +27,44 @@ module.exports = function(params) {
         },
         bail: true,
         module: {
-            loaders: [
-                {
-                    test: /\.jsx?$/,
-                    exclude: /(node_modules(\\|\/)(?!(impl|ut)\-).)/,
-                    loader: 'babel',
-                    query: {
-                        presets: ['es2015', 'stage-0', 'react', 'react-hmre'],
-                        cacheDirectory: true
-                    }
-                },
-                {test: /\.json$/, loader: 'json'},
-                {test: /.*\.(gif|png|jpe?g|svg)$/i, loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}']},
-                {test: /\.css$/, loaders: ['style?sourceMap', 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]']}
-            ]
+            loaders: [{
+                test: /\.jsx?$/,
+                exclude: /(node_modules(\\|\/)(?!(impl|ut)\-).)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-0', 'react', 'react-hmre'],
+                    cacheDirectory: true
+                }
+            }, {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&minetype=application/font-woff'
+            }, {
+                test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            }, {
+                test: /\.json$/,
+                loader: 'json'
+            }, {
+                test: /.*\.(gif|png|jpe?g|svg)$/i,
+                loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+                ]
+            }, {
+                test: /\.css$/,
+                loaders: ['style?sourceMap', 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]']
+            }]
         },
         plugins: [
-            new webpack.SourceMapDevToolPlugin({filename: 'bundle.js.map', moduleFilenameTemplate: '[absolute-resource-path]', fallbackModuleFilenameTemplate: '[absolute-resource-path]'}),
+            new webpack.SourceMapDevToolPlugin({
+                filename: 'bundle.js.map',
+                moduleFilenameTemplate: '[absolute-resource-path]',
+                fallbackModuleFilenameTemplate: '[absolute-resource-path]'
+            }),
             // new webpack.optimize.DedupePlugin(),
-            new webpack.IgnorePlugin(/^(app|browser\-window|global\-shortcut|crash\-reporter|protocol|dgram|JSONStream|inert|hapi|socket\.io|winston|async|bn\.js|engine\.io|url|glob|mv|minimatch|stream-browserify|browser-request)$/),
+            new webpack.IgnorePlugin(
+                /^(app|browser\-window|global\-shortcut|crash\-reporter|protocol|dgram|JSONStream|inert|hapi|socket\.io|winston|async|bn\.js|engine\.io|url|glob|mv|minimatch|stream-browserify|browser-request)$/
+            ),
             new BellOnBundlerErrorPlugin()
         ]
     };
 };
-
