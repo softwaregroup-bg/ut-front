713fa268b7176588de2cc5e63a758ef22060d71a
Alexander Seferinkyn <alexandar.seferinkin@softwaregroup-bg.com>
2016-06-16 14:50:38 +0300

    fix(PermissionCheck): function to check permissions improved

--- react/permissions.js
+++ react/permissions.js
@@ -1,15 +1,31 @@
-var permissions = {data: []};
-
-function matcher(required) {
-    return !!~permissions.data.indexOf(required);
-};
+var permissions = null;
+var cache = {};
 
 export const check = function(actions) {
-    var _actions = Array.isArray(actions) ? actions : [actions];
-    return _actions.filter(matcher).length === _actions.length;
+    if (!Array.isArray(actions)) {
+        actions = [actions];
+    }
+
+    for (let i = 0; i < actions.length; i++) {
+        let action = actions[i];
+        let value = cache[action];
+
+        if (value === undefined) {
+            let has = cache[action] = permissions.test(action);
+            if (has) {
+                return true;
+            }
+        } else if (value === true) {
+            return true;
+        }
+    }
+
+    return false;
 };
 
 export const set = function(list) {
-    permissions = {data: []};
-    permissions.data = list;
+    cache = {};
+    permissions = new RegExp(list.map(function(permission) {
+        return permission.actionId.replace('%', '(.+?)');
+    }).join('|'));
 };
