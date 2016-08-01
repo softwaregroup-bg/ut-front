2c269fa00fb3aca2d6064122806f967da93a2185
Alexander Seferinkyn <alexandar.seferinkin@softwaregroup-bg.com>
2016-08-01 21:42:54 +0300

    feat(action-promise): added promise to the action
    Backend actions now have 'promise' property which is created from ut-bus ajax system. This allows the action creators to subscribe for the ajax request lifecycle events.

--- react/middleware.js
+++ react/middleware.js
@@ -3,32 +3,43 @@ import thunk from 'redux-thunk';
 export default (utBus) => {
     const rpc = (store) => (next) => (action) => {
         if (action.method) {
-            utBus
-                .importMethod(action.method)(action.params)
-                .then((r) => {
-                    action.result = r;
-                })
-                .catch((e) => {
-                    action.error = e;
-                })
-                .finally(() => {
-                    action.methodRequestState = 'finished';
-                    next(action);
-                });
             action.methodRequestState = 'requested';
+
+            // This is to prevent circular references (and potential memory leak): action -> promise -> action
+            // "As of 2012, all modern browsers ship a mark-and-sweep garbage-collector"
+            // However, we can't rely on the user that he doesn't use browser with reference-counting garbage-collector
+            const resultAction = {
+                result: undefined,
+                error: undefined,
+                ...action
+            };
+
+            action.promise = utBus
+                .importMethod(action.method)(action.params)
+                .then(result => {
+                    resultAction.result = result;
+                })
+                .catch(error => {
+                    resultAction.error = error;
+                })
+                .then(() => {
+                    resultAction.methodRequestState = 'finished';
+                    return next(resultAction);
+                });
         }
         return next(action);
     };
 
     const utBuslogger = (store) => (next) => (action) => {
         if (action.method) {
-            // log the action
+            // TODO: log the action
             return next(action);
         } else if (action.type === 'UT_LOG') {
-            // log the action
+            // TODO: log the action
             return action;
         }
         return next(action);
     };
+
     return [thunk, rpc, utBuslogger];
 };
