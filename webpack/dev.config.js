f1122840ea4c389dabae5065c217d3ff8a094c22
Anton Bozhinov <anton.bozhinov@softwaregroup-bg.com>
2016-10-21 09:12:30 +0300

    fix: merge

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -57,7 +57,7 @@ module.exports = (params) => ({
         modules: ['node_modules', 'dev'] // https://github.com/webpack/webpack/issues/2119#issuecomment-190285660
     },
     module: {
-        exprContextCritical: false,
+        exprgContextCritical: false,
         loaders: [
             {
                 test: /\.jsx?$/,
