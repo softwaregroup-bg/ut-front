d544fbc7714a71a3f7d7c4f374dd99efc6837ae0
elin.angelov <elin.angelov@softwaregroup-bg.com>
2016-11-08 11:19:03 +0200

    fix: working config

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -19,64 +19,10 @@ module.exports = (params) => ({
     resolve: {
         modules: ['node_modules', 'dev'] // https://github.com/webpack/webpack/issues/2119#issuecomment-190285660
     },
-    closures: {
-        translate: function(config) {
-            return new Promise((resolve, reject) => {
-                if (this.translateResult) {
-                    resolve(this.translateResult);
-                    return;
-                }
-                if (this.loading) {
-                    return this.translate(config);
-                }
-                this.loading = true;
-                if (config && config.language) {
-                    return params.languages().then((languages) => {
-                        var languageId = languages[0].filter((language) => {
-                            return language.iso2Code === config.language;
-                        });
-                        var translateParams = {};
-                        if (languageId[0]) {
-                            translateParams = {
-                                languageId: languageId[0].languageId
-                            };
-                        }
-                        return params.translate(translateParams).then((result) => {
-                            this.translateResult = result;
-                            return resolve(result);
-                        });
-                    });
-                } else {
-                    return params.translate().then((result) => {
-                        this.translateResult = result;
-                        return resolve(result);
-                    });
-                }
-            });
-        }
-    },
     plugins: [
         new webpack.IgnorePlugin(
             /^(app|browser\-window|global\-shortcut|crash\-reporter|protocol|dgram|JSONStream|inert|hapi|socket\.io|winston|async|bn\.js|engine\.io|url|glob|mv|minimatch|stream-browserify|browser-request)$/
         ),
-        // Injects options object per loader (Webpack 2 specific)
-        new webpack.LoaderOptionsPlugin({
-            /*
-               PostCSS configuration: list of plugins used to gain features and functionality
-               postcss-import: can consume files at given paths and inline them when requested in other css files (should be first in the array if possible)
-             */
-            options: {
-                postcss: [
-                    require('postcss-import')({
-                        addDependencyTo: webpack,
-                        // where to look for files when @import [filename].css is used in another css file
-                        path: [(params.themePath || ''), (params.configPath || '')]
-                    }),
-                    // Transforms CSS specs into more compatible CSS so you don’t need to wait for browser support
-                    require('postcss-cssnext')
-                ]
-            }
-        }),
         new webpack.DefinePlugin(params.sharedVars),
         new BellOnBundlerErrorPlugin()
     ],
@@ -100,7 +46,7 @@ module.exports = (params) => ({
             loader: 'url?limit=30720000'
         }, {
             test: /\.css$/,
-            loader: 'style?sourceMap!css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss-loader'
+            loader: 'style?sourceMap!css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
         }]
     }
 });
