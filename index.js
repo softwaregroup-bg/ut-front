61bee5f81a55a87ae2acafb449d7fe01b3ec69ab
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2018-11-27 18:41:15 +1000

    fix: refactor as function
    BREAKING CHANGE: require upgrade of ut-run

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const path = require('path');
 var serverRequire = require;
 
-module.exports = function(moduleConfig) {
-    var bus;
-
-    var result = {
-        init: function(b) {
-            bus = b;
+module.exports = function utFront({utBus, config}) {
+    return {
+        init: function() {
             this.cachePath = {};
         },
         start: function() {
             var redirectTo = path.basename(this.config.entryPoint, '.js');
             this.cachePath = path.resolve(
                 ((this.config.packer && this.config.packer.name) ? this.config.packer.cachePath : this.config.dist) ||
-                path.join(bus.config.workDir, 'ut-front', this.config.id));
+                path.join(utBus.config.workDir, 'ut-front', this.config.id));
 
             // do index route
             var indexRoute = {
@@ -52,12 +49,12 @@ module.exports = function(moduleConfig) {
                     title: '@title!',
                     entryPoint: this.config.entryPoint,
                     jsxExclude: this.config.packer.jsxExclude
-                                ? this.config.packer.jsxExclude.constructor.name === 'RegExp'
-                                    ? this.config.packer.jsxExclude
-                                    : new RegExp(this.config.packer.jsxExclude)
-                                : /(node_modules(\\|\/)(?!(.*impl|.*ut|.*dfsp)-).)/,
-                    themePath: moduleConfig.themePath,
-                    configPath: moduleConfig.configPath,
+                        ? this.config.packer.jsxExclude.constructor.name === 'RegExp'
+                            ? this.config.packer.jsxExclude
+                            : new RegExp(this.config.packer.jsxExclude)
+                        : /(node_modules(\\|\/)(?!(.*impl|.*ut|.*dfsp)-).)/,
+                    themePath: config.themePath,
+                    configPath: config.configPath,
                     postcssLoader: this.config.postcssLoader,
                     cssImport: this.config.cssImport,
                     cssAssets: this.config.cssAssets
@@ -79,5 +76,4 @@ module.exports = function(moduleConfig) {
             return globalRoute;
         }
     };
-    return result;
 };
